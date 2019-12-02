6141a4591b4e685b443bdbe9e477aef259b83008
ZheWeiShu <50991532+ZheWeiShu@users.noreply.github.com>
2019-12-02 21:27:29 +0800

    2019/12/2
    角色動作匯入

---
+++ Assets/程式/Raycast.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    public bool fireRay;
+    [SerializeField]private float longueur;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        if (fireRay)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, longueur * Time.deltaTime);
+            Debug.DrawRay(transform.position, -Vector2.up * longueur * Time.deltaTime, Color.red, 2);
+            if (hit.collider.CompareTag("地面"))
+            {
+                fireRay = false;
+            }
+        }
+    }
+   
+}
