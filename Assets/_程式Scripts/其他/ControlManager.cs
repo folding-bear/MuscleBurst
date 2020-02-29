892cfcca59ebd693b4b5b7e3164ca93a91b494a1
ZheWeiShu <50991532+ZheWeiShu@users.noreply.github.com>
2020-02-29 20:12:53 +0800

    2020 2/29

--- Assets/_程式Scripts/其他/ControlManager.cs
+++ Assets/_程式Scripts/其他/ControlManager.cs
@@ -9,6 +9,8 @@ public class ControlManager : MonoBehaviour
     public Text[] buttons = new Text[4];
     private readonly Array keyCodes = Enum.GetValues(typeof(KeyCode));
     public PlayerBasicAction player;
+    
+    public bool choose { get; set; }
     void Start()
     {
         //player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBasicAction>();
@@ -21,59 +23,53 @@ public class ControlManager : MonoBehaviour
 
     void Update()
     {
-        
+        ChangeConrol();
+        //Debug.Log(choose);
     }
     
-    public void ChangeConrol(int index)
+    private void ChangeConrol()
     {
-        //Event e = Event.current;
-        //if (e.isKey)
-        //{
-        //    switch (index)
-        //    {
-        //        case 0:
-        //            player.up = e.keyCode;
-        //            buttons[0].text = e.keyCode.ToString();
-        //            break;
-        //        case 1:
-        //            player.down = e.keyCode;
-        //            break;
-        //        case 2:
-        //            player.left = e.keyCode;
-        //            break;
-        //        case 3:
-        //            player.right = e.keyCode;
-        //            break;
-        //    }
-        //}
-        if (Input.anyKeyDown)
+        if (choose)
         {
+            if (Input.anyKeyDown)
+            {
             foreach (KeyCode keyCode in keyCodes)
             {
                 if (Input.GetKey(keyCode))
                 {
-                    switch (index)
-                    {
-                        case 0:
-                            player.up = keyCode;
-                            buttons[0].text = keyCode.ToString();
-                            break;
-                        case 1:
-                            player.down = keyCode;
-                            buttons[1].text = keyCode.ToString();
-                            break;
-                        case 2:
-                            player.left = keyCode;
-                            buttons[2].text = keyCode.ToString();
-                            break;
-                        case 3:
-                            player.right = keyCode;
-                            buttons[3].text = keyCode.ToString();
-                            break;
-                    }
+                //Debug.Log(""+keyCode);
+                        player.up = keyCode;
+                        buttons[0].text = keyCode.ToString();
+                    //    switch (index)
+                    //{
+                    //    case 0:
+                    //        //buttons[0].text.Equals(keyCode.ToString());
+                    //        player.up = keyCode;
+                    //        buttons[0].text = keyCode.ToString();
+                    //        break;
+                    //    case 1:
+                    //        player.down = keyCode;
+                    //        buttons[1].text = keyCode.ToString();
+                    //        break;
+                    //    case 2:
+                    //        player.left = keyCode;
+                    //        buttons[2].text = keyCode.ToString();
+                    //        break;
+                    //    case 3:
+                    //        player.right = keyCode;
+                    //        buttons[3].text = keyCode.ToString();
+                    //        break;
+                    //}
+                    choose = false;
                     break;
                 }
             }
+            }
         }
+
+    }
+   public void ChooseButton()
+    {
+       choose = true;
     }
 }
